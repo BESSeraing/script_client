8fd5b3f0dc4fb1df0600670f195ba885562dc451
kokmok <jona303@gmail.com>
2018-12-11 12:27:32 +0100

    jquery 1

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -1,77 +1,80 @@
-//KeyboardsEvent
-var speed = 1;
-var directionX =0;
-var directionY =1;
+//Attendre la disponibilité du dom
+// Peut s'écrire en raccourci $(function(){
+$(document).ready(function(){
+    addClassExemple();
+    createBody();
+    checkboxExemple();
+    fadeExemple();
+    styleIt();
+    loopIt();
+    positionIt();
 
-document.getElementById("ptitBoulRouge").style.left = "10px";
-document.getElementById("ptitBoulRouge").style.top = "50px";
-document.addEventListener('keydown',keyDown);
+});
 
-var interval = setInterval(clock,1000/60);
+function positionIt(){
+    console.log('position de la boule rouge en left : ' +$('#ptitBoulRouge').position().left);
+    console.log('position de la boule rouge en top : ' +$('#ptitBoulRouge').position().top);
+    console.log('offset de la boule rouge en left : ' +$('#ptitBoulRouge').offset().left);
+    console.log('offset de la boule rouge en top : ' +$('#ptitBoulRouge').offset().top);
+}
 
-function clock(){
-    console.log('interval');
+function loopIt(){
+    $('.menuLi').each(function(index,elem){  //Boucle sur tous les éléments contenus dans le selecteur
+        //Premier argument : itération, deuxième: element courant
+        $(elem).html("menu n°"+index);
+    })
+}
 
-    var posX = parseInt(document.getElementById("ptitBoulRouge").style.left)+directionX;
-    var posY = parseInt(document.getElementById("ptitBoulRouge").style.top)+directionY;
 
-    document.getElementById("ptitBoulRouge").style.left = posX+"px";
-    document.getElementById("ptitBoulRouge").style.top = posY+"px";
+function styleIt(){
+    $('#mainMenu')
+        .css("margin-top","100px")
+        .css({
+            "margin-bottom":"100px",
+            "text-transform":"uppercase"
+            })
+        .css('background-color',"#00FF99") //On "chaîne", ça veut dire qu'on peut appeller plusieurs methodes à la suite sue le même object
+    ;
+
+    console.log($('#mainMenu').html());
+}
+
+function fadeExemple(){
+    $('.fadeIn').fadeIn(10000,function(){
+        $(this).slideUp(1000);
+    });
+}
+
+
+function checkboxExemple(){
+    var id = $('input[type=checkbox]:checked')
+        .addClass('soChecked')
+        .attr('id');
+    console.log("la checkbox checkée a l'id :"+id);
+}
+
+
+function createBody(){
+    var mainObject = $('<div id="main"></div>');
+    mainObject.html("ceci est de l'html");
+    $('header').after(mainObject); //On insère l'object après le <header>
+}
+
+
+function addClassExemple(){
+    //Sélecteur jQuery
+    var links = $('a');
+    links.addClass('superLink');
 }
 
 
 
-
-function keyDown(e){
-    console.log("code touche down : "+e.keyCode);
-
-    switch (e.keyCode){
-        case 37:
-            directionX -= speed;
-            break;
-        case 39:
-            directionX += speed;
-            break;
-        case 40:
-            directionY += speed;
-            break;
-        case 38:
-            directionY -= speed;
-            break;
-    }
-
-
+/*
+//Façon de faire sans jQuery
+var nativeLinks = document.getElementsByTagName('a');
+for(var i = 0;i<nativeLinks.length;i++){
+    nativeLinks[i].className += " superLink2";
 }
-
-//Bubble vs capture
-document.getElementById("container").addEventListener("click",clickedContainer);
-document.getElementById("red").addEventListener("click",clickedRed);
+*/
 
 
-function clickedRed(e){
-    console.log('RED CLICKED');
-    console.log(e.type);
-    e.preventDefault();
-
-}
-
-function clickedContainer(e){
-    console.log('Container CLICKED');
-}
-
-//Basic event
-// document.getElementById('myA').addEventListener("click",clicked);
-//
-//
-//
-//
-// function clicked(e){
-//     // e.preventDefault();
-//     alert("CLIIIIIIIICK");
-//     // console.log(e);
-// }
-
-
-function log(varName,value){
-    console.log('la variable '+varName+' contient la valeur : '+value);
-}
